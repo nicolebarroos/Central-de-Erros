6b75320bfea9a0cae8adabe23b85763f95341f14
Nicole <barrosnicole094@gmail.com>
2020-07-11 18:55:47 -0300

    add categoria

--- erros/models.py
+++ erros/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from datetime import datetime
 
+class Categoria(models.Model):
+    status = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=50)
+
 class Erros(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     level = models.CharField(max_length=50)
     decricao = models.CharField(max_length=100)
     data_hora = models.DateTimeField(default=datetime.now)
     eventos = models.CharField(max_length=50)
-
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
